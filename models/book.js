0495f9baf94cfe317ff93844cc9530d9b83712e3
nursetta <n_ursetta@yahoo.com>
2016-07-06 15:58:06 -0600

    final sprint 1

---
+++ models/book.js
@@ -0,0 +1,14 @@
+// book.js
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+     title: String,
+     author: String,
+     image: String,
+     releaseDate: String
+    // you should fill the rest of this in
+});
+
+var Book = mongoose.model('Book', BookSchema);
+module.exports = Book;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 // SERVER-SIDE JAVASCRIPT
 
-
+var db = require('./models');
 /////////////////////////////
 //  SETUP and CONFIGURATION
 /////////////////////////////
@@ -25,29 +25,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //  DATA
 ///////////////////
 
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
 
 
 
@@ -67,11 +44,13 @@ app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
 });
 
-// get all books
+// server.js
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+  db.Book.find(function(err, books){
+    if (err) { return console.log("index error: " + err); }
+    res.json(books);
+  });
 });
 
 // get one book
